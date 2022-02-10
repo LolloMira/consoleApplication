68b2b98c1d04ec73c0f23c73d8d533ca9b0968ac
LolloMira <lorenzo23082000@gmail.com>
2022-02-10 11:30:24 +0100

    klkk

--- app.js
+++ app.js
@@ -1,11 +1,82 @@
 "use strict"
 
-const { log } = require('console');
 const fs = require('fs')
 
+class Parser {
+
+  static csvParse(string) {
+      if (string.length === 0  ) {
+          throw new EmptyStringError("Stringa vuota")
+      }
+      const tempString = Parser.ReplaceAll(string);
+      const tempArray = Parser.Splitter(tempString); 
+      const newArray = Parser.checkParse(tempArray);
+      if (newArray[0].length === 0) {
+          throw new InvalidStringError("Stringa Invalida");
+      } else if (newArray[0].length > 0 && newArray[1]){
+          throw new PartialInvalidStringError("Stringa Parzialmente Valida", newArray[0])
+      }
+      return newArray[0];
+  }
+
+  static ReplaceAll(string){
+      let stringNumber = string;
+      if (string.includes(",")) {
+          return stringNumber.replace(/,/g, ".");
+      }
+      return stringNumber;
+  }
+
+  static Splitter(string){
+      return string.split("; ");
+  }
+
+  static checkParse(array){
+      let newArray = [];
+      let flagNaN = false;
+      for (let i = 0; i < array.length; i++) {
+          let numbers = parseFloat(array[i])
+          if (!isNaN(numbers)) {
+              newArray.push(numbers);
+          } else{
+              flagNaN = true;
+          }
+      }
+      let passArray = [newArray, flagNaN]
+      return passArray;
+  }
+}
+
+class EmptyStringError extends Error{
+  constructor(message){
+      super(message);
+  }
+}
+
+class InvalidStringError extends Error{
+  constructor(message){
+      super(message);
+  }
+}
+
+
+class PartialInvalidStringError extends Error{
+constructor(message, array){
+  super(message)
+  this.array = array;
+}
+
+}
+
 try {
-    const data = fs.readFileSync('./test.csv', 'utf8')
+    const data = fs.readFileSync('./test2.csv', 'utf8')
+    const array = Parser.csvParse(data);
     console.log(data);
+    console.log(array);
+
+    let sum = array.reduce((p,c)=>p+c)
+
+    console.log(sum);
 } catch (err) {
   console.error(err);
 }
